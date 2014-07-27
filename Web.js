b2a781db8854e35ee8a91b9a013cdfe203f83c4a
Jitendra Zaa <jitendra.zaa@gmail.com>
2014-07-26 23:31:25 -0400

    Fixing Issues

--- Web.js
+++ Web.js
@@ -3,10 +3,19 @@ var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + '/client'));
+
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+app.all('/proxy', function(req, res) {
+    var url = req.header('SalesforceProxy-Endpoint');
+    console.log("proxying: " + url);
+    request({ url: url, method: req.method, json: req.body, headers: {'Authorization': req.header('X-Authorization')} }).pipe(res);
+});
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
