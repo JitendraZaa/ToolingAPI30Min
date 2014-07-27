50f20435c2094ce91405a88c7c5b020b0c456efc
Jitendra Zaa <jitendra.zaa@gmail.com>
2014-07-26 23:27:06 -0400

    Changed to Simple Code

--- Web.js
+++ Web.js
@@ -1,28 +1,14 @@
-var express = require('express'),
-    http = require('http'),
-    request = require('request'),
-    app = express();
-
+var express = require("express");
 var logfmt = require("logfmt");
+var app = express();
 
- 
 app.use(logfmt.requestLogger());
-/*app.use(express.static(__dirname + '/client'));*/
-
-/*
-app.all('/proxy', function(req, res) {
-    var url = req.header('SalesforceProxy-Endpoint');
-    console.log("proxying: " + url);
-    request({ url: url, method: req.method, json: req.body, headers: {'Authorization': req.header('X-Authorization')} }).pipe(res);
-});
-*/
 
 app.get('/', function(req, res) {
-	  res.send('Hello World!');
-	});
+  res.send('Hello World!');
+});
 
-app.set('port', process.env.PORT || 3000);
-
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
