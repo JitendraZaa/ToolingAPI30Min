9198bc0d3a5b617951c656ab389dbf0daa25ad40
Jitendra Zaa <'Jitendra.zaa@gmail.com'>
2014-10-11 01:21:00 -0400

    Added logic to change http to https

--- client/js/OAuth.js
+++ client/js/OAuth.js
@@ -5,8 +5,8 @@ $("#sandBtn").click(sandLogin);
 var apiVersion = 'v30.0',
     clientId = '3MVG9iTxZANhwHQuSJa6AuCgpr0Lu3QNRNKk4c2FejzTys5Mlp43UeSHBuhWWgRjEUyV6xE7N0GostjR3sRat',
     loginUrl = 'https://login.salesforce.com/',
-    redirectURI = "http://toolingapi.herokuapp.com/oauthcallback.html",
-    proxyURL = 'http://toolingapi.herokuapp.com/proxy/',
+    redirectURI = "https://toolingapi.herokuapp.com/oauthcallback.html",
+    proxyURL = 'https://toolingapi.herokuapp.com/proxy/',
     client = new forcetk.Client(clientId, loginUrl, proxyURL);
 
  
